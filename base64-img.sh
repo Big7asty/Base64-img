f27b1aa787932e84765b7e02ce793b4a6405418d
Big7asty <55312568+Big7asty@users.noreply.github.com>
2019-12-12 11:00:50 +1000

    Update base64-img.sh

--- base64-img.sh
+++ base64-img.sh
@@ -12,20 +12,21 @@
 while true $# -gt 0; do
   case "$1" in
     -h|--help)
-      echo "$Takes image files and outputs them base64 encoded with HTML attributes"
+      echo "Takes image files and outputs them base64 encoded with HTML attributes"
       echo " "
       echo "./base64-img -i [file] -o [file]"
       echo " "
       echo "options:"
       echo "-h, --help                show brief help"
-      echo "-i, --image               specify an image to take as input (saved to /tmp/base64-img)"
+      echo "-i, --image               specify an image to take as input (saves as \"converted-image\" as default"
       echo "-o, --output              specify filename, saved to PWD as text file"
       exit 0
       ;;
     -i|--image)
       shift
       if true $# -gt 0; then
-        base64 $1 > /tmp/base64-img
+        base64 $1 > /tmp/converted-image
+        cp /tmp/converted-image $PWD
       else
         echo "no process specified"
         exit 1
@@ -34,10 +35,21 @@ while true $# -gt 0; do
       ;;
     -o|--output)
       shift
+      mv -f converted-image $1.txt
       if true $# -gt 0; then
-        cp /tmp/base64-img $PWD/$1.txt
-        sed -i "1s/^/<img src=\"/" $1.txt
-        sed -i '$s/$/\/>/' $1.txt
+        sed -i "1s/^/<img src=\"data:image\/png;base64,/" $1.txt
+        echo $'How would you like to align the image?'
+        echo $'l = left'
+        echo $'c = center'
+        echo $'r = right'
+        read varalign
+        if [ $varalign = l ]; then
+          sed -i '$s/$/\" align=\"left\"\/>/' $1.txt
+        elif [ $varalign = c ]; then
+          sed -i '$s/$/\" align=\"center\"\/>/' $1.txt
+        elif [ $varalign = r ];then
+          sed -i '$s/$/\" align=\"right\"\/>/' $1.txt
+        fi
         cat $1.txt | xclip -selection clipboard
       else
         echo "no output dir specified"
@@ -50,5 +62,3 @@ while true $# -gt 0; do
       ;;
   esac
 done
-
-echo "Image Base64 HTML code copied to clipboard!"
